dcc28a6e700244fd41b08bc190237c7c848d4608
Ben Kirby and Paige Williams <ben@>
2019-01-09 15:13:31 -0800

    Added functionality to calculate day of week.

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,12 +1,59 @@
 import { Calculator } from './../src/calculator.js';
 
 describe('Calculator', function(){
+  let date;
+  let anotherDate;
+  let anExtraDate;
+
+  beforeEach(function(){
+    date = new Calculator("2017-04-09");
+    anotherDate = new Calculator("2016-04-09");
+    anExtraDate = new Calculator("2016-01-17");
+  });
+
 
   it('takes a data and stores it as a new date object', function(){
-    let newDate = new Calculator("2019-01-09");
-    console.log(newDate.inputDate.getUTCDate());
-    expect(newDate.inputDate.getUTCFullYear()).toEqual(2019);
-    expect(newDate.inputDate.getUTCMonth()).toEqual(0);
-    expect(newDate.inputDate.getUTCDate()).toEqual(9);
+    expect(date.inputDate.getUTCFullYear()).toEqual(2017);
+    expect(date.inputDate.getUTCMonth()).toEqual(3);
+    expect(date.inputDate.getUTCDate()).toEqual(9);
+
+    expect(anotherDate.inputDate.getUTCFullYear()).toEqual(2016);
+    expect(anotherDate.inputDate.getUTCMonth()).toEqual(3);
+    expect(anotherDate.inputDate.getUTCDate()).toEqual(9);
+
+  });
+
+
+  it('finds the number of leap years between the anchor date and the inputted date ', function(){
+    expect(date.leapYearBetween()).toEqual(489);
+    expect(anotherDate.leapYearBetween()).toEqual(489);
+
+  });
+
+  it('finds the number of days in total completed months for the current year', function(){
+    expect(date.monthsBetween()).toEqual(90);
+    expect(anotherDate.monthsBetween()).toEqual(91);
+  });
+
+  it('finds the number of days in total years between anchor date', function(){
+    expect(date.daysInCompletedYears()).toEqual(736331);
+    expect(anotherDate.daysInCompletedYears()).toEqual(735965);
+    expect(anExtraDate.daysInCompletedYears()).toEqual(735965);
+  });
+
+  it('finds the number of days in total years, months and days between inputted date and anchor date', function(){
+    expect(date.calculateTotalDays()).toEqual(42832);
+    expect(anotherDate.calculateTotalDays()).toEqual(42467);
+    expect(anExtraDate.calculateTotalDays()).toEqual(42384);
+
+
+  });
+
+  it('finds the day of the week for a given date and returns it as a string', function(){
+    expect(date.dayOfWeek()).toEqual("Sunday");
+    expect(anotherDate.dayOfWeek()).toEqual("Saturday");
+    expect(anExtraDate.dayOfWeek()).toEqual("Sunday");
+
+
   });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,90 @@
 class Calculator {
   constructor(inputDate){
-    this.anchorDate = new Date("1900-01-01");
+    this.anchorDate = new Date("0001-01-01");
     this.inputDate = new Date(inputDate);
+    this.isLeapYear = false;
   }
+
+  isInputLeapYear(){
+    if ((this.inputDate.getUTCFullYear() % 4 === 0) && (this.inputDate.getUTCFullYear() % 100 !== 0)) {
+      this.isLeapYear = true;
+    }
+    else if (this.inputDate.getUTCFullYear() % 400 === 0) {
+      this.isLeapYear = true;
+    }
+  }
+
+
+  leapYearBetween() {
+    this.isInputLeapYear();
+    const anchorYear = this.anchorDate.getUTCFullYear();
+    const inputYear = this.inputDate.getUTCFullYear();
+    // const yearsBetween = inputYear - anchorYear;
+    let leapYears = 0;
+    for (var i = anchorYear; i <= inputYear; i++) {
+      if ((i % 4 === 0) && (i % 100 !== 0)) {
+        leapYears++;
+      }
+      else if (i % 400 === 0) {
+        leapYears++;
+      }
+    }
+    return leapYears;
+  }
+
+  daysInCompletedYears(){
+    const yearsBetween = this.inputDate.getUTCFullYear() - this.anchorDate.getUTCFullYear();
+    console.log(yearsBetween);
+    const totalLeapYears = this.leapYearBetween();
+    const normalYearsBetween = yearsBetween - totalLeapYears;
+    let totalDaysInYears;
+
+    if (this.isLeapYear === true) {
+      totalDaysInYears = ((366 * totalLeapYears) - 1) + (365 * normalYearsBetween);
+    }
+    else if (this.isLeapYear === false) {
+      totalDaysInYears = (366 * totalLeapYears) + (365 * normalYearsBetween);
+    }
+    console.log(totalDaysInYears);
+    console.log(this.isLeapYear);
+    return totalDaysInYears;
+  }
+
+  monthsBetween(){
+    this.isInputLeapYear();
+    const currentMonth = this.inputDate.getUTCMonth();
+    const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+    let daysInWholeMonths = 0;
+
+    for (var i = 0; i < currentMonth; i++) {
+      daysInWholeMonths += months[i]
+    }
+
+    if ((this.isLeapYear === true) && (currentMonth > 1)) {
+      daysInWholeMonths++;
+      return daysInWholeMonths;
+    }
+    else {
+      return daysInWholeMonths;
+    }
+  }
+
+  daysBetween(){
+    this.isInputLeapYear();
+    const days = this.inputDate.getUTCDate() - 1;
+    return days;
+  }
+
+  calculateTotalDays(){
+    const totalDays = this.daysInCompletedYears() + this.monthsBetween() + this.daysBetween();
+    return totalDays;
+  }
+
+  dayOfWeek() {
+    const weekday =["Monday", "Tuesday", "Wednesday",  "Thursday", "Friday", "Saturday", "Sunday"];
+
+    return weekday[this.calculateTotalDays() % 7];
+  }
+
 }
 export { Calculator };
